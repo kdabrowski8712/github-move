f15594fb0c8a83484fe22d216488cb6b0a493928
k.dabrowski8712 <k.dabrowski8712@gmail.com>
2019-02-23 17:22:57 +0100

    submodule 19.3

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -18,8 +19,18 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById(Long id) {
-        return repository.findTaskById(id);
+    public Optional<Task> getTask(Long id) {
+
+        return repository.findById(id);
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+
+        repository.delete(id);
     }
 
 }
